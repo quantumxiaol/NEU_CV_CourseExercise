20dd9daf669e72ad76913d6aa0e0d82859854f1e
APLiu <75256935+quantumxiaol@users.noreply.github.com>
2024-02-28 10:27:02 +0800

    task3

---
+++ task3/lung/mox.py
@@ -0,0 +1,8 @@
+import moxing as mox
+from config import cfg
+
+mox.file.copy_parallel(src_url=cfg.OUTPUT_DIR,
+                           dst_url='obs://lqy/img-segment-main/output_train/unet++')
+
+mox.file.copy_parallel(src_url=cfg.SUMMARY_DIR,
+                          dst_url='obs://lqy/img-segment-main/summary_log/unet++')
